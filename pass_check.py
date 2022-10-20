73d4c0088bf376ad53432e020a5116609a63a445
MOLIzhang <93513340+MOLIzhang@users.noreply.github.com>
2022-10-21 00:50:55 +0800

    Update pass_check.py

--- pass_check.py
+++ pass_check.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-req = requests.get('这里填金数据页面')
+req = requests.get('URL')
 req.encoding = "utf-8"
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
@@ -9,7 +9,7 @@ company_item = soup.find('tr',{'data-field':"field_31"})
 dd = company_item.text.strip().replace("\n","").replace(" ","")
 
 if dd == '通过情况已通过':
-    requests.get('https://api.day.app/这里填barktoken/自由行申请已通过')
+    requests.get('https://api.day.app/barktoken/自由行申请已通过')
 else:
     print()
 
