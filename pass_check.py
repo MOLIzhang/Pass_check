50c87c872c30e2b6d1bed9bf0632947ecfab85ab
MOLIzhang <93513340+MOLIzhang@users.noreply.github.com>
2022-10-21 01:13:49 +0800

    Update pass_check.py
    添加secret

--- pass_check.py
+++ pass_check.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-req = requests.get('URL')
+URL = os.environ["URL"]
+BARKTOKEN = os.environ["BARKTOKEN"]
+
+req = requests.get('%s' % URL)
 req.encoding = "utf-8"
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
@@ -9,7 +12,7 @@ company_item = soup.find('tr',{'data-field':"field_31"})
 dd = company_item.text.strip().replace("\n","").replace(" ","")
 
 if dd == '通过情况已通过':
-    requests.get('https://api.day.app/barktoken/自由行申请已通过')
+    requests.get('https://api.day.app/%s/自由行申请已通过' % BARKTOKEN)
 else:
     print()
 
